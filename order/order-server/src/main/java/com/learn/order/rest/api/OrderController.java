3462aab3705b673846bebcdcf41fb4ea09fc3180
tanglingyun <345684180@qq.com>
2018-05-27 03:16:26 +0800

    自动配置刷新

--- order/order-server/src/main/java/com/learn/order/rest/api/OrderController.java
+++ order/order-server/src/main/java/com/learn/order/rest/api/OrderController.java
@@ -1,5 +1,18 @@
 package com.learn.order.rest.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learn.order.config.OrderCofing;
 import com.learn.order.converter.OrderForm2OrderDTO;
 import com.learn.order.dto.OrderDTO;
 import com.learn.order.enums.ResultEnum;
@@ -8,17 +21,8 @@ import com.learn.order.rest.form.OrderForm;
 import com.learn.order.service.OrderService;
 import com.learn.product.dto.ResultDTO;
 import com.learn.product.util.ResultDTOUtil;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.CollectionUtils;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("order")
@@ -27,9 +31,12 @@ public class OrderController {
 
 	@Autowired
 	private OrderService orderService;
+	@Autowired
+	private OrderCofing orderCofing;
 
 	@PostMapping("create")
 	public ResultDTO<?> create(@Valid OrderForm orderForm, BindingResult bindingResult) {
+		log.info("配置中心配置对象orderCofing={}",orderCofing);
 		if (bindingResult.hasErrors()) {
 			log.error("请求创建订单 参数校验错误:", orderForm);
 			throw new OrderException(ResultEnum.PARAM_ERROR.getCode(),bindingResult.getFieldError().getDefaultMessage());
