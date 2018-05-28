38191c291b1a48382cbca0e8e74c7a572ca1e06d
tanglingyun <345684180@qq.com>
2018-05-28 23:40:21 +0800

    修改版本号 集群应用变基为单机

--- product/product-server/src/main/java/com/learn/product/rabbitmq/RabbitMQRecver.java
+++ product/product-server/src/main/java/com/learn/product/rabbitmq/RabbitMQRecver.java
@@ -11,19 +11,19 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class RabbitMQRecver {
 	
-	@RabbitListener(queues = "user.login.queue")
-	public void userLoginQueueListener(@Payload String message,CorrelationData correlationData) throws Exception{
-		log.info("该用户完成了登录:{}",message);
-	}
-	
-	@RabbitListener(queues = "headers.queue")
-	public void headersQueueListener(@Payload byte[] message) throws Exception{
-		log.info("header:{}",new String(message));
-	}
-	
-	@RabbitListener(queues = "product.kill.queue")
-	public void productKillQueueListener(@Payload String message) throws Exception{
-		log.info("该用完成了秒杀:{}",message);
-	}
+//	@RabbitListener(queues = "user.login.queue")
+//	public void userLoginQueueListener(@Payload String message,CorrelationData correlationData) throws Exception{
+//		log.info("该用户完成了登录:{}",message);
+//	}
+//
+//	@RabbitListener(queues = "headers.queue")
+//	public void headersQueueListener(@Payload byte[] message) throws Exception{
+//		log.info("header:{}",new String(message));
+//	}
+//
+//	@RabbitListener(queues = "product.kill.queue")
+//	public void productKillQueueListener(@Payload String message) throws Exception{
+//		log.info("该用完成了秒杀:{}",message);
+//	}
 	
 }
