fa10c7126d6fe7f19c39579baa3f2d38976f4883
TangLingYun <345684180@qq.com>
2018-05-28 13:53:47 +0800

    整合项目启动方式(war启动)

--- product/product-server/src/main/java/com/learn/product/ProductApplication.java
+++ product/product-server/src/main/java/com/learn/product/ProductApplication.java
@@ -2,12 +2,21 @@ package com.learn.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProductApplication {
+public class ProductApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ProductApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
 	}
+
 }
